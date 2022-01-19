594db361da98107166cfc2b877b7edc3e9357cf3
jeongyeonkim <wjddus9132@gmail.com>
2022-01-19 19:23:00 +0900

    propTypes example

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,8 +1,10 @@
-const MyComponent = (props) => {
+import PropTypes from "prop-types";
+
+const MyComponent = ({ name, children }) => {
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}
       입니다.
     </div>
   );
@@ -10,6 +12,10 @@ const MyComponent = (props) => {
 
 MyComponent.defaultProps = {
   name: "기본 이름",
+};
+
+MyComponent.propTyped = {
+  name: PropTypes.string,
 };
 
 export default MyComponent;
