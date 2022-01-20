fd919dde55338837de8f30c73f6f6d9bf270339b
jeongyeonkim <wjddus9132@gmail.com>
2022-01-20 20:20:58 +0900

    class type component state

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,28 @@
-import { render } from '@testing-library/react';
-import { Component } from 'react';
+import { render } from "@testing-library/react";
+import { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 0
-        };
-    }
-}
-render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+  render() {
     const { number } = this.state;
     return (
-        <div>
-            <h1>{number}</h1>
-            <button 
-                onClick={() => {
-                    this.setState({number: number + 1});
-                }}
-            >
-                +1
-            </button>
-        </div>
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
     );
+  }
 }
 
 export default Counter;
