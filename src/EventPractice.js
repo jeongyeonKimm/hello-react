854f237aa16144e5f5a986f70b287c907affa5d8
jeongyeonkim <wjddus9132@gmail.com>
2022-01-21 22:56:39 +0900

    event practice

---
+++ src/EventPractice.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤틑 연습</h1>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
